86eaf1425cd6e14103630a7e148e13f3dcbb1cc3
Olexandr <olexandrcv@gmail.com>
2023-03-07 23:26:26 +0200

    Homework 10

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,2 +1,66 @@
+from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
 
-# Register your models here.
+from currency.models import Rate, ContactUs, Source
+
+
+@admin.register(Rate)
+class RateAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sell',
+        'currency',
+        'source',
+        'created',
+    )
+    list_filter = (
+        'currency',
+        ('created', DateRangeFilter)
+    )
+    search_fields = (
+        'source',
+        'buy',
+        'sell',
+    )
+
+    readonly_fields = (
+        'buy',
+        'sell',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email_from',
+        'subject',
+        'message',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'source_url',
+        'name',
+        'note',
+    )
