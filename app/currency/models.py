34bb5d3a39ec4d1e6b4a6c3c007c8e126171545e
Olexandr <olexandrcv@gmail.com>
2023-03-12 14:37:17 +0200

    Homework 11

--- app/currency/models.py
+++ app/currency/models.py
@@ -18,6 +18,8 @@ class Rate(models.Model):
 
 
 class ContactUs(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=128)
     email_from = models.EmailField()
     subject = models.CharField(max_length=50)
     message = models.CharField(max_length=255)
@@ -33,3 +35,9 @@ class Source(models.Model):
 
     def __str__(self):
         return f'Source url: {self.source_url}'
+
+
+class RequestResponseLog(models.Model):
+    path = models.CharField(max_length=255)
+    request_method = models.CharField(max_length=10)
+    time = models.DecimalField(max_digits=30, decimal_places=20)
